5da8edb8a7ceb1f3df294bc7bcee82b871e8813c
MarkovicMihajlo <mihajlomarkovic003@gmail.com>
2025-03-12 22:53:56 +0100

    Make movie model, and showing all movies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'gallery', component: GalleryComponent },
     { path: '**', redirectTo: '' }
 ];
---
+++ src/models/movie.model.ts
@@ -0,0 +1,36 @@
+export interface MovieModel{
+
+    movieId : number
+    director: {
+        directorId: number,
+        name: string,
+        createdAt : string
+    }
+    originalTitle : string
+    description : string
+    shortDescription : string
+    runTime : number
+    startDate : string
+    poster : string
+    movieActors : Array<{
+            movieActorId: number,
+            movieId: number,
+            actorId: number,
+            actor: {
+                actorId: number,
+                name: string,
+                createdAt: string
+            }
+    }>
+    movieGenres : Array<{
+            movieGenreId: number,
+            movieId: number,
+            genreId: number,
+            genre: {
+                    genreId: number,
+                    name: string,
+                    createdAt: string
+             }
+    }>    
+
+}
