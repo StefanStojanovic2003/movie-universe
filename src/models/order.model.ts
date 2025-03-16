67333712ec35261d3b9d785ede117d87af4e3406
MarkovicMihajlo <mihajlomarkovic003@gmail.com>
2025-03-16 01:28:32 +0100

    order model changed

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,7 +1,10 @@
+import { CinemasModel } from "./cinemas.model"
+
 export interface OrderModel{
     title: string
     startDate: string
     runTime: string
+    cinema: CinemasModel
     count: number
     pricePerItem: string
     status: 'ordered' | 'watched' | 'canceled'
